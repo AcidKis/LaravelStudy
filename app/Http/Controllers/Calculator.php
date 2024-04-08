1a7e2a593c684c6bf022657d0b3fa5b86fb53785
Andrey Kiselev <kisand.work@mail.ru>
2024-04-08 20:58:12 +0300

    first init

---
+++ app/Http/Controllers/Calculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Calculator extends Controller
+{
+    public function index()
+    {
+        return 1;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Calculator;
+
+Route::get('/', [Calculator::class, 'index']);
